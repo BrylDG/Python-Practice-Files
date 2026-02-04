b89babd088bdc6e20e6ba78fa26dcf06b2221a06
DarelBG <brylgorgonio@gmail.com>
2026-02-05 00:38:18 +0800

    Exercise#3 Arithmetic Functions

---
+++ exercise3.py
@@ -0,0 +1,7 @@
+import math
+
+radius = float(input("Enter radius: "))
+
+print(f"The circumference of the circle is: {2+math.pi*radius}cm")
+
+print(f"The are of the circle is: {math.pi * pow(radius, 2)}cm")
