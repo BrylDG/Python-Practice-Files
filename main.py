0c9b979382b023c40f421c2a3d4a0c32b1ac5633
DarelBG <brylgorgonio@gmail.com>
2026-02-04 23:51:32 +0800

    First python exercise

---
+++ main.py
@@ -0,0 +1,11 @@
+user_name = "Bryl Darel Gorgonio"
+year = 2003
+pi = 3.14
+is_admin = False
+
+if is_admin:
+    print(f"Your username is {user_name}")
+    print(f"You are born on the year {year}")
+    print(f"Pi value is {pi}")
+else:
+    print("You need to be an admin")
